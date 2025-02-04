79c952b9018fba973514387750f856754d23d7dd
Joe Orchard <JO278870@falmouth.ac.uk>
2025-02-04 12:52:32 +0000

    Added random walk code

---
+++ Assets/Scripts/SCR_PCGAlgorithms.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***************************************************************************************
+*    Title: Random Walk Algorithm - P4 - Unity Procedural Generation of a 2D Dungeon
+*    Author: Sunny Valley Studio - Peter https://www.youtube.com/@SunnyValleyStudio
+*    Date: 18/12/2020
+*    Availability: https://www.youtube.com/watch?v=F_Zc1nvtB0o&list=PLcRSafycjWFenI87z7uZHFv6cUG2Tzu9v&index=4
+*
+***************************************************************************************/
+
+public static class SCR_PCGAlgorithms
+{
+    public static HashSet<Vector2Int> RandomWalk(Vector2Int StartPos, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(StartPos);
+
+        var previousPos = StartPos;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPos + Direction2D.GetRandomDirection();
+            path.Add(newPosition);
+            previousPos = newPosition;
+        }
+        return path;
+    }
+}
+
+//Class for access to simple direction data
+public static class Direction2D
+{
+    public static List<Vector2Int> directionsList = new List<Vector2Int>()
+    {
+        new Vector2Int(0, 1), //UP
+        new Vector2Int(1, 0), //RIGHT
+        new Vector2Int(0, -1), //DOWN
+        new Vector2Int(-1, 0) //LEFT
+    };
+
+    public static Vector2Int GetRandomDirection()
+    {
+        return directionsList[Random.Range(0, directionsList.Count)];
+    }
+}
